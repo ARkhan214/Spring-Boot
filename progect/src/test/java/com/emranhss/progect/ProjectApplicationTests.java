97561ed65c5bf3342277f32ec7f9e65ee638202f
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-07-16 19:01:22 +0600

    add spring

---
+++ progect/src/test/java/com/emranhss/progect/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emranhss.progect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
